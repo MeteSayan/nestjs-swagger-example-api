8dbd23c336065c7c512fbc4a38ae8169c022a607
Mete Sayan <metesayan99@gmail.com>
2023-10-31 22:53:51 +0300

    feat: Demo resources generated with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DemoModule } from './demo/demo.module';
 
 @Module({
-  imports: [],
+  imports: [DemoModule],
   controllers: [AppController],
   providers: [AppService],
 })
