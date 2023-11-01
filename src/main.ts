e0f876fdf8d9e937847dde1d7785086d70bbd8c7
Mete Sayan <metesayan99@gmail.com>
2023-11-01 21:48:34 +0300

    feat: Nestjs configuration & Swagger module impl.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  const apiPath = 'api';
+  app.setGlobalPrefix(apiPath);
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest-js E-commerce API')
+    .setDescription('E-commerce API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(`${apiPath}/docs`, app, document);
+
+  await app.listen(3200);
 }
 bootstrap();
